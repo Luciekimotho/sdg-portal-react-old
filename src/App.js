f692adb9dbc2a4c1e6a9dac545aeac9c22e3d16d
Luciekimotho <luciekimotho@gmail.com>
2019-11-20 14:19:07 +0300

    Initialized React app, added styling template and bootstrap

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import "./assets/vendor/nucleo/css/nucleo.css";
+import "./assets/vendor/font-awesome/css/font-awesome.min.css";
+import "./assets/scss/argon-design-system-react.scss";
 
 function App() {
   return (
