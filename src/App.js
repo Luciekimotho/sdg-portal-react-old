9989f621838640cf4802399d94a18539d38ef0bb
Luciekimotho <luciekimotho@gmail.com>
2019-11-20 17:29:59 +0300

    Added header component

--- src/App.js
+++ src/App.js
@@ -4,24 +4,14 @@ import './App.css';
 import "./assets/vendor/nucleo/css/nucleo.css";
 import "./assets/vendor/font-awesome/css/font-awesome.min.css";
 import "./assets/scss/argon-design-system-react.scss";
+import './App.css';
+
+import Header from "./components/header.js"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header></Header>
     </div>
   );
 }
